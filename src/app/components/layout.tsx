b00fee08023d8df978b1aba6e5a13f7cf84cab5e
Nhat Duy <pnhatduy0409@gmail.com>
2024-03-30 10:10:24 +0700

    any type children

--- src/app/components/layout.tsx
+++ src/app/components/layout.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Header from './header';
 import Footer from './footer';
 
-const Layout = ({ children }) => {
+const Layout = ({ children }: {children: React.ReactNode}) => {
     return (
         <div>
             <Header />
